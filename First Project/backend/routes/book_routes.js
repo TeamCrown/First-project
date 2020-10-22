028518e30dbb86078252754c77665c3e7ff1f74d
Minasie Alemu <minalem.5d@gmail.com>
2020-10-22 20:15:36 +0300

    [backend] API Implemented!

--- First Project/backend/routes/book_routes.js
+++ First Project/backend/routes/book_routes.js
@@ -11,6 +11,55 @@ router.get("/", async(req, res)=>{
     }
 });
 
+router.get("/:bookId", async (req, res)=>{
+    try{
+        const id = req.params.bookId;
+        const book = await Book.findById(id);
+        if (book) {
+            res.status(200).json(book);
+        } else {
+            res.status(404).json({message: "No Valid entry found"});
+        }
+    } catch (e) {
+        res.status(500).json({message:e});
+    }
+});
+
+router.delete("/:bookId",async (req, res)=>{
+    try{
+        const id = req.params.bookId;
+        await Book.delete(id);
+        Book.updateOne()
+        res.status(200).json({message: "Book Deleted successfully!"});
+    } catch (e) {
+        res.status(500).json({message:e});
+    }
+} );
+
+router.update("/:bookId", async (req, res)=>{
+    try{
+        const id = req.params.bookId;
+        const book = Book.findById(id);
+        const updated = new Book({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            description: req.body.description,
+            coverImage: req.body.coverImage,
+            language: req.body.language,
+            review: req.body.review,
+        });
+        if (book){
+            await Book.updateOne({_id:id,$set: updated});
+            res.status(200).json()
+        } else {
+            res.status(404).json({message: "No Valid entry found"});
+        }
+    } catch (e) {
+        res.status(500).json({message:e});
+    }
+})
+
 router.post("/", async (req, res)=>{
     const myBook = new Book({
         title: req.body.title,
