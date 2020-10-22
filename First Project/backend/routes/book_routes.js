b66c4c85e0e48b0f4f9dccae2df78881d8a540c7
Minasie Alemu <minalem.5d@gmail.com>
2020-10-22 21:32:12 +0300

    [backend].Final API Implementation!

--- First Project/backend/routes/book_routes.js
+++ First Project/backend/routes/book_routes.js
@@ -28,37 +28,32 @@ router.get("/:bookId", async (req, res)=>{
 router.delete("/:bookId",async (req, res)=>{
     try{
         const id = req.params.bookId;
-        await Book.delete(id);
-        Book.updateOne()
+        await Book.findByIdAndDelete(id);
         res.status(200).json({message: "Book Deleted successfully!"});
     } catch (e) {
         res.status(500).json({message:e});
     }
 } );
 
-router.update("/:bookId", async (req, res)=>{
-    try{
+router.put("/:bookId", (req, res)=>{
+   
         const id = req.params.bookId;
-        const book = Book.findById(id);
-        const updated = new Book({
-            title: req.body.title,
-            author: req.body.author,
-            genre: req.body.genre,
-            description: req.body.description,
-            coverImage: req.body.coverImage,
-            language: req.body.language,
-            review: req.body.review,
-        });
-        if (book){
-            await Book.updateOne({_id:id,$set: updated});
-            res.status(200).json()
-        } else {
-            res.status(404).json({message: "No Valid entry found"});
-        }
-    } catch (e) {
-        res.status(500).json({message:e});
-    }
-})
+        
+            Book.findByIdAndUpdate(id,req.body,{
+                author: req.body.author,
+                genre: req.body.genre,
+                description: req.body.description,
+                coverImage: req.body.coverImage,
+                language: req.body.language,
+                review: req.body.review,
+            }, (err,doc)=>{
+                if(err){
+                    res.status(500).json({message:"Error updating the book"});
+                }else{
+                    res.status(200).json({message:"book updated"});
+                }
+            });
+});
 
 router.post("/", async (req, res)=>{
     const myBook = new Book({
